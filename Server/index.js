d3bee7ac73114b192f0d759820fa0d857fc07243
gabrielagi <gabrielagi503@gmail.com>
2023-09-23 17:09:11 -0300

    Project base structure

---
+++ Server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const server = express();
+
+const { SERVER_PORT } = process.env;
+
+server.listen(SERVER_PORT, () => {
+  console.log(`Server is listening on port: ${SERVER_PORT}`);
+});
