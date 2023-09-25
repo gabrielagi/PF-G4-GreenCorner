9faa3cc7652118e49c0e26f205502f9337d23340
gabrielagi <gabrielagi503@gmail.com>
2023-09-25 11:06:49 -0300

    Test change Home without pull

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Home (){
-  return(
+export default function Home() {
+  return (
     <div>
       <h1>Home</h1>
+      <h3>Este es el Home</h3>
     </div>
-  )
+  );
 }
-
