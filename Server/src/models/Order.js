5675561cbc8d9ebf9513490901a4383e80f70046
Fakuese2 <fakuese2@hotmail.com>
2023-09-25 17:28:24 -0300

    OrderModel-ready

--- Server/src/models/Order.js
+++ Server/src/models/Order.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
 
     //   },
       total:{
-        type:DataTypes.INTEGER,
+        type:DataTypes.DECIMAL,
         allowNull:false
       },
       //user ID
