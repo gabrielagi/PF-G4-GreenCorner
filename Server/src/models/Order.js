93eff82817cb3a5c0f40e1149d397fe2193c5f42
andresmdkr <andresmdkr@gmail.com>
2023-09-25 17:44:18 -0300

    Hotfix

--- Server/src/models/Order.js
+++ Server/src/models/Order.js
@@ -1,35 +1,37 @@
-module.exports = (sequelize, DataTypes) => {
-   sequelize.define('Order', {
-      id: {
-        type: DataTypes.UUIDV4,
-        primaryKey: true,
-        unique:true, 
-      },
-      date: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      status:{
-        type:DataTypes.ENUM('Pending','Finish','Rejected'),
-        allowNull:false,
-      },
-      shippingAdress:{
-        type:DataTypes.STRING,
-        allowNull:false
-      },
-      adressHouseNumber:{
-        type:DataTypes.INTEGER,
-        allowNull:false
-      },
-    //   paymenMethod:{
+const { DataTypes } = requiere('sequelize');
+module.exports = (sequelize) => {
+  sequelize.define('Order', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      unique: true,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE, 
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('Pending', 'Finish', 'Rejected'),
+      allowNull: false,
+    },
+    shippingAddress: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    addressHouseNumber: { 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    total: {
+      type: DataTypes.DECIMAL(10, 2), 
+      allowNull: false,
+    },
+    // user ID 
+  });
 
-    //   },
-      total:{
-        type:DataTypes.DECIMAL,
-        allowNull:false
-      },
-      //user ID
-    } );
-    
-  };
+
+};
+
   
