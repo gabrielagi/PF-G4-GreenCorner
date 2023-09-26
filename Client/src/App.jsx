997ae0535c052bae99c91c5d603b17563c397b72
gabrielagi <gabrielagi503@gmail.com>
2023-09-26 15:52:06 -0300

    Navbar Profile (complete)

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,28 +1,23 @@
-import { Routes, Route, } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import "tailwindcss/tailwind.css";
 import NavbarUser from "./components/Navbar/Navbar.user";
-import Home from "./pages/Home"
-import Detail from "./pages/Detail"
-
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
 const App = () => {
-  
-  return(
-  
+  return (
     <div>
       <NavbarUser />
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/detail/:id' element={<Detail />} />
-          {/* <Route path='/shop' element={< />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        {/* <Route path='/shop' element={< />} />
           <Route path='/favourites' element={<Favourites />} /> */}
-
-        </Routes>
-
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
