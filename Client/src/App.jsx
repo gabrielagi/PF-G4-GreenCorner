bc47b79d77022b841416652308d4a2dc81b35011
AveMat81 <matiasavesani@yahoo.com.ar>
2023-09-26 14:26:14 -0300

    Route + store + reducer base + main.jsx config

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,38 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route, } from 'react-router-dom';
 import "./App.css";
-
 import "tailwindcss/tailwind.css";
 import NavbarUser from "./components/Navbar/Navbar.user";
+import Home from "./pages/Home"
+import Detail from "./pages/Detail"
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+const App = () => {
+  
+  return(
+  
+    <div>
       <NavbarUser />
-    </>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/detail/:id' element={<Detail />} />
+          {/* <Route path='/shop' element={< />} />
+          <Route path='/favourites' element={<Favourites />} /> */}
+
+        </Routes>
+
+    </div>
   );
 }
 
