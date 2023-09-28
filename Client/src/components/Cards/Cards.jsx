35604d154a2e5d1a2b3bc0b551fe6a28b0afd2eb
AveMat81 <matiasavesani@yahoo.com.ar>
2023-09-28 12:59:18 -0300

    update Cards

--- Client/src/components/Cards/Cards.jsx
+++ Client/src/components/Cards/Cards.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-//import { getAllProducts }from "../../Redux/actions/action"
+import { getAllProducts } from "../../Redux/actions/product/action"
 import Card from "./Card/Card";
 
 
