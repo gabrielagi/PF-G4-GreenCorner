c18ca6cb98b07d1be7e2336690ac8b7d5833af77
AveMat81 <matiasavesani@yahoo.com.ar>
2023-09-28 15:48:01 -0300

    Adjustments to Cards and responsive design

--- Client/src/components/Cards/Cards.jsx
+++ Client/src/components/Cards/Cards.jsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useSelector } from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { getAllProducts } from "../../Redux/actions/product/action"
 import Card from "./Card/Card";
 
 
-const allProducts = ({ allProducts }) => {
 
+const AllProducts = () => {
+
+    const products = useSelector(state=>state.allProducts)
     const dispatch = useDispatch();
 
     useEffect(()=>{
@@ -14,18 +16,17 @@ const allProducts = ({ allProducts }) => {
     },[dispatch]);
 
     return(
-        <div>
-               {allProducts.map((p, i)=>{
-                return(
-                    <div key={i}>
-                      {/* nombre del preducto,
-                      image,
-                      precio */}
-                    </div>
-                )
-               })}              
-        </div>
+        <div className="flex flex-wrap ml-4">
+        {products.map((p, i)=>{
+            return <Card
+                key={i}
+                name={p.name}
+                image={p.image}
+                price={p.price}                
+            />
+        })}
+    </div>
        
 )};
    
-export default allProducts
+export default AllProducts
