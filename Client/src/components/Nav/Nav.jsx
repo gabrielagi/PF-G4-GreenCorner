97f050944273901ebf5bb18579371197038fe342
andresmdkr <andresmdkr@gmail.com>
2023-09-28 13:31:51 -0300

    feat(navbar): added responsive design

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -1,20 +1,24 @@
 import "../Nav/Nav.css";
 import { CiUser } from "react-icons/ci";
 import { GrCart } from "react-icons/gr";
-import { GrFormSearch } from "react-icons/gr";
+import { GrSearch } from "react-icons/gr";
 import LoginButton from "../Auth0/LoginButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "../Auth0/LogoutButton";
+import {ImMenu3} from "react-icons/im";
 
 const Nav = () => {
   const { isAuthenticated } = useAuth0();
+ 
 
   return (
     <nav className="nav">
       <a href="/" className="nav__brand">
         GreenCorner 🍃
       </a>
+    
       <ul className="nav__menu">
+        
         <li className="nav__item">
           <a href="/" className="nav__link">
             <div className="home">Home</div>
@@ -31,7 +35,7 @@ const Nav = () => {
           </a>
         </li>
         <li className="nav__item">
-          <a href="about-us" className="nav__link">
+          <a href="about-us" className="nav__link ">
             About Us
           </a>
         </li>
@@ -42,14 +46,17 @@ const Nav = () => {
         </li>
       </ul>
       <div className="nav__toggler">
-    <a href="#">
-        <GrFormSearch style={{ fontSize: "24px" }}/>
-    </a>
-    <a href="#">
-        <GrCart style={{ fontSize: "24px" }}/>
-    </a>
-    {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+  <a href="#">
+    <GrSearch style={{ fontSize: "24px" }} />
+  </a>  
+  
+  <a href="#">
+    <GrCart style={{ fontSize: "24px" }} />
+  </a>
+  
+  {isAuthenticated ? <LogoutButton /> : <LoginButton />}
 </div>
+
     </nav>
   );
 };
