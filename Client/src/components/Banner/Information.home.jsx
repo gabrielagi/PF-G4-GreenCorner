9d199aeeb9346d7c6b66c6bc8766a1ec1d8dee39
gabrielagi <gabrielagi503@gmail.com>
2023-09-28 11:17:21 -0300

    Home banner section + Information structure (section 2)

--- Client/src/components/Banner/Information.home.jsx
+++ Client/src/components/Banner/Information.home.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const InformationHome = () => {
   return (
     <div className="w-full">
-      <div className="max-w-[1480px] m-auto grid md:grid-cols-2"></div>
+      <div className="max-w-[1480px] m-auto grid md:grid-cols-2">
+        <h1 className="text-center text-2xl font-bold text-[#47a358]">
+          Trusted by over 25,000 people 
+        </h1>
+        <p></p>
+      </div>
       <p>Hola Information</p>
     </div>
   );
