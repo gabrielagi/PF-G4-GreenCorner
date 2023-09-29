93606f01640cf57205a6b0302cff3fef88d78ebf
andresmdkr <andresmdkr@gmail.com>
2023-09-29 01:37:52 -0300

    chore: final touch-ups and fixes for Shop component, demo

--- Client/src/components/Cards/Cards.jsx
+++ Client/src/components/Cards/Cards.jsx
@@ -1,31 +1,23 @@
-
-import {  useSelector} from "react-redux";
-import { Link } from "react-router-dom";
-
 import Card from "./Card/Card";
+import plantgif from "../../assets/plantgif.gif";
 
-
-
-const Cards = (allProducts) => {
-
-
-    return(
-        <div className="flex flex-wrap ml-4">
-        {allProducts.map((p, i)=>{
-             return   <Link to={`/detail/${p.product_id}`} key={i}>
-                <Card
-                key={i}
-                name={p.name}
-                image={p.image}
-                price={p.price}                
-            />
-                </Link>
-
-             
-        })}
-        
+const Cards = ({ allProducts }) => {
+  return (
+    <div className="flex flex-wrap ml-4">
+      {allProducts ? (
+        allProducts.map((p, i) => {
+         return (
+              <Card key={i} name={p.name} image={p.image} price={p.price} id={p.product_id} />
+          );
+        })
+      ) : (
+        <div className="flex justify-center items-center">
+          <img src={plantgif} alt="loading" />
+        </div>
+      )}
     </div>
-       
-)};
-   
-export default Cards
+  );
+};
+
+export default Cards;
+
--- Client/src/pages/Shop/Shop.jsx
+++ Client/src/pages/Shop/Shop.jsx
@@ -27,10 +27,6 @@ function handleOrder(e){
       e.preventDefault();
       dispatch(filterByPrice(e.target.value))
   }
- /*  if(e.target.value === "all"){
-      e.preventDefault();
-      dispatch(resetProducts(e.target.value))
-  } */
 }
 
   return (
@@ -40,7 +36,7 @@ function handleOrder(e){
   onChange={(e) => handleOrder(e)}
   className={styles.order}
   >
-  <option disabled selected>Order</option>
+  <option disabled selected>Order ⮟</option>
   <option value="asc">A - Z</option>
   <option value="desc">Z - A</option>
   
@@ -49,7 +45,7 @@ function handleOrder(e){
   onChange={(e) => handleOrder(e)}
   className={styles.order}
   >
-  <option disabled selected>Price</option>
+  <option disabled selected>Price ⮟</option>
   <option value="high">High - Low</option>
   <option value="low">Low - High</option>
 </select>
