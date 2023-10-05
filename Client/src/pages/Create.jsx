195a858f7aeb7877d1d466c48a8552158de24dbe
Fakuese2 <fakuese2@hotmail.com>
2023-10-05 18:50:39 -0300

    create que sea lo q dios quiera

--- Client/src/pages/Create.jsx
+++ Client/src/pages/Create.jsx
@@ -1,87 +1,13 @@
-<<<<<<< HEAD
+
 // import {useForm} from 'react-hook-form'
 // import  { useState, useEffect } from "react";
 // import axios from "axios";
 // import { useDispatch, useSelector } from 'react-redux';
 // import { addProduct, getAllProducts, getAllCategories } from '../Redux/actions/product/action'
-=======
+
 import React, { useState } from "react";
->>>>>>> 3cdf156c3da99a6e6154bdf16d066302d13d471d
-
-import loadingGif from "../assets/loading.gif";
-
-<<<<<<< HEAD
-// const Create=() =>{
-//   const {register, handleSubmit }= useForm()
-//   const categories = useSelector(state => state.categories)
-//   const [productData, setProductData] = useState({
-//     name: "",
-//     price: "",
-//     stock: "",
-//     categories: null,
-//     images: []
-//   });
-  
-//   const dispatch = useDispatch()
-  
-//   useEffect(() => {
-//     dispatch(getAllProducts());
-//     dispatch(getAllCategories());
-//   }, [dispatch]);
 
 
-//   const handleInputChange = (event) => {
-    
-//     const { name, value } = event.target;
-//     if (name === "categories") {
-//       const selectedCategories = Array.from(event.target.selectedOptions).map(option => option.value);
-//       const updatedCategories = (productData.categories || []).concat(selectedCategories);
-//       setProductData({ ...productData, categories: updatedCategories });
-//     } else {
-//       setProductData({ ...productData, [name]: value });
-//     }
-//   };
-
-  
-
-//   return <div>
-//      <h2>añade un producto</h2>
-//     <form> 
-//       <div>
-//       <label>Titulo</label>
-//       <input type='text' name='titulo'></input>
-//       </div>
-
-//       <div>
-//       <label>Precio</label>
-//       <input type='number' name='precio'></input>
-//       </div>
-
-//       <div>
-//       <label>Imagenes</label>
-//       <input type='file' name='imagenes'></input>
-//       </div>
-
-//       <div>
-//       <label>Stock</label>
-//       <input type='number' name='stock'></input>
-//       </div>
-
-//       <div>
-//       <label>Categories</label>
-//       <select>
-//         {categories.map((c)=>{(<option value=''></option>)} )}
-        
-//       </select>
-//       </div>
-//     </form>
-
-//   </div>
-
-
-// }
-
-// export default Create
 
 
 
@@ -238,85 +164,10 @@ const Create = () => {
         <button type="submit">Submit</button>
       </form>
 
-      {productData.images.length > 0 && (
-        <div>
-          <h2>Archivos seleccionados:</h2>
-          <ul>
-            {productData.images.map((file, index) => (
-              <li key={index}>{file.name}</li>
-            ))}
-          </ul>
-=======
-const cloudName = "dc9wewxge";
-const preset = "green_corner";
-
-const Create = () => {
-  const [image, setImage] = useState(""); // Guarda la URL de la imagen que trae Cloudinary
-  const [loading, setLoading] = useState(false); // Controla que se muestre el GIF mientras carga la imagen en Cloudinary
-  const [newProduct, setNewProduct] = useState({});
-
-  const cloudinaryUrl = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
-
-  // Método para hacer la transformación de la URL imagen a Cloudinary
-  const uploadImage = async (event) => {
-    console.log("File sin recortar: ", event.target.files);
-    const fileUpload = event.target.files[0]; // Obtener el primer archivo seleccionado
-    console.log("Esto es el archivo cargado: ", fileUpload);
-
-    const data = new FormData();
-    data.append("file", fileUpload);
-    data.append("upload_preset", preset);
-    data.append("cloud_name", cloudName);
-
-    console.log(data);
-    setLoading(true);
-
-    try {
-      const result = await fetch(cloudinaryUrl, { method: "POST", body: data });
-      // const result = await axios.post(cloudinaryUrl, data);
-      console.log("Result de llamado a Cloudinary", result);
-
-      if (!result.ok) {
-        throw new Error("Error al cargar la imagen a Cloudinary");
-      }
-
-      const response = await result.json();
-      console.log("URL de la imagen de Cloudinary: ", response.secure_url); // URL de la imagen en Cloudinary
-
-      setImage(response.secure_url);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error:", error);
-      setLoading(false);
-    }
-  };
-
-  // Método handleSubmit
-
-  return (
-    <div>
-      <h1>Subir Imagen</h1>
-      <form>
-        <label>Titulo de producto:</label>
-        <input type="text" name="title" />
-        {/* Comienza la parte de subir imagen y convertirla */}
-        <input
-          type="file"
-          name="file"
-          placeholder="Subir una imagen"
-          onChange={uploadImage}
-        />
-      </form>
-      {loading && <img src={loadingGif} />}
-      {image && (
-        <div>
-          <h2>Imagen Subida:</h2>
-          <img src={image} alt="Imagen Subida" width="300" />
->>>>>>> 3cdf156c3da99a6e6154bdf16d066302d13d471d
         </div>
       )}
-    </div>
-  );
-};
+
+
+
 
 export default Create;
