934b273506d0c076e9e426b6d93f1cd9a56e7d21
gabrielagi <gabrielagi503@gmail.com>
2023-10-05 16:06:20 -0300

    chore(payment): Update Mercado Pago integration and preferences

--- Server/src/Controller/payment.controller.js
+++ Server/src/Controller/payment.controller.js
@@ -10,68 +10,94 @@ const HOST = `http://${DB_HOST}:${SERVER_PORT}/payment`;
 mercadopago.configure({
   access_token: ACCESS_TOKEN,
 });
-
 const createOrder = async (req, res) => {
   // El product puede ser un objeto individual desde Detail o un array desde Cart
-  const product = req.body.product;
-  const amount = req.body.amount || 1; // Si amount no es enviado asumo un valor predeterminado en 1
+  // const product = req.body.product;
+  // const amount = req.body.amount || null; // Si amount no es enviado asumo un valor predeterminado en 1
 
-  // Guardo los items que se van a vender
-  let itemsToSell = [];
+  // // Guardo los items que se van a vender
+  // let itemsToSell = [];
 
-  // Si viene de Detail es un objeto
-  let convertProdcutFromDetail = {};
-  if (typeof product === "object") {
-    convertProdcutFromDetail = {
-      id: product.id,
-      quantity: amount,
-      title: product.name,
-      unit_price: product.price,
+  // // Si viene de Detail es un objeto
+  // let convertProdcutFromDetail = {};
+  // if (typeof product === "object") {
+  //   convertProdcutFromDetail = {
+  //     id: product.id,
+  //     quantity: amount,
+  //     title: product.name,
+  //     unit_price: product.price,
+  //     currency_id: "ARS",
+  //   };
+  // }
+
+  // // Verifico si es un producto individual o un conjunto de Productos
+  // convertProdcutFromDetail
+  //   ? itemsToSell.push(convertProdcutFromDetail)
+  //   : product.forEach((product) =>
+  //       itemsToSell.push({
+  //         id: product.id,
+  //         quantity: product.amount,
+  //         title: product.name,
+  //         unit_price: product.price,
+  //         currency_id: "ARS",
+  //       })
+  //     );
+
+  // Creo los items para la preferencia
+  // let payer = {
+  //   name: user.given_name,
+  //   surname: user.family_name,
+  // };
+
+  const items = [
+    {
+      id: 3,
+      quantity: 5,
+      title: "Laptop",
+      unit_price: 500,
       currency_id: "ARS",
-    };
-  }
-
-  // Verifico si es un producto individual o un conjunto de Productos
-  convertProdcutFromDetail
-    ? itemsToSell.push(convertProdcutFromDetail)
-    : product.forEach((product) =>
-        itemsToSell.push({
-          id: product.id,
-          quantity: amount,
-          title: product.name,
-          unit_price: product.price,
-          currency_id: "ARS",
-        })
-      );
-
-  // Datos del usuario que realiza la compra
-  let payer = {
-    name: user.given_name,
-    surname: user.family_name,
-  };
-
-  const result = await mercadopago.preferences.create({
-    items: itemsToSell,
-    // URLs de redirección
-    back_urls: {
-      success: `${HOST}/success`,
-      failure: `${HOST}/failure`,
-      pending: `${HOST}/pending`, // Cuando el usuario no ha pagado
     },
-    // Es cuando el pago ha terminado
-    notification_url: "https://fb4d-190-97-120-13.ngrok.io/payment/webhook",
-    auto_return: "approved",
-  });
+  ];
 
-  console.log("Result de Create Order", result);
+  try {
+    const result = await mercadopago.preferences.create({
+      payer_email: "test_user_1398180221@testuser.com",
+      //payer: payer,
+      items,
+      // URLs de redirección
+      back_urls: {
+        success: `${HOST}/success`,
+        failure: `${HOST}/failure`,
+        pending: `${HOST}/pending`, // Cuando el usuario no ha pagado
+      },
+      // Es cuando el pago ha terminado
+      notification_url: "https://fb4d-190-97-120-13.ngrok.io/payment/webhook",
+      auto_return: "approved",
+    });
 
-  res.send("creando orden");
+    console.log("Result de Create Order", result);
+
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Methods", "POST");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.set("Access-Control-Allow-Credentials", true);
+    console.log("URL: ", result.body.init_point);
+    res.json({
+      global: result.body.id,
+      // Identificador único que puedes utilizar para referenciar y gestionar esa preferencia en futuras interacciones con la API de Mercado Pago
+    });
+    console.log("----Fin del Create Order------------");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const success = (req, res) => {
   console.log(req.query);
   // res.send('Pago realizado')
-  res.redirect("http://localhost:5173/");
+  res.redirect("http://localhost:5173/"); // Agregar componente notificación para redirigir
 };
 
 const receiveWebhook = async (req, res) => {
@@ -80,12 +106,13 @@ const receiveWebhook = async (req, res) => {
 
   try {
     // Pregunto si la venta es correcta y la respuesta es payment
-    if (payment.type === "payment") {
-      const data = await mercadopago.payment.findById(["data.id"]);
+    if (payment.type === "payment" || payment.type === "payment") {
+      const data = await mercadopago.payment.findById(payment.id);
       console.log("Data del Webhook", data);
 
       // store in database
       // Puedo guadar la información del usuario una vez que compró
+      // Actualizar cantidad de productos en el Stock de los productos vendidos
     }
     res.status(204); // Significa que todo salió bien pero no devuelve nada
   } catch (error) {
