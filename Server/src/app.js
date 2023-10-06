e9831d3c0e14d304ea0411c35c40d7bd45b6a32b
andresmdkr <andresmdkr@gmail.com>
2023-10-06 10:08:55 -0300

    deploy(main)

--- Server/src/app.js
+++ Server/src/app.js
@@ -17,7 +17,7 @@ server.use(cookieParser());
 server.use(cors());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://pf-g4-green-corner-git-develop-gabrielas-projects-79d3d559.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://green-corner.vercel.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
