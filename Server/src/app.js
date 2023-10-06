5a76287df848b6a6195ee7fce2f7ed1347e2d8c6
andresmdkr <andresmdkr@gmail.com>
2023-10-06 09:05:08 -0300

    fix

--- Server/src/app.js
+++ Server/src/app.js
@@ -17,7 +17,7 @@ server.use(cookieParser());
 server.use(cors());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
