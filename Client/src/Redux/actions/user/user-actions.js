86c0af8b777743e81af884e504f3bcbee0feca5e
andresmdkr <andresmdkr@gmail.com>
2023-10-10 12:44:39 -0300

    .

--- Client/src/Redux/actions/user/user-actions.js
+++ Client/src/Redux/actions/user/user-actions.js
@@ -13,8 +13,8 @@ import {
 
 import axios from "axios";
 
-const endpoint = `https://greencorner.onrender.com/user`;
-/* const endpoint = "http://localhost:3001/user"; */
+/* const endpoint = `https://greencorner.onrender.com/user`; */
+const endpoint = "http://localhost:3001/user";
 
 export const getFavorites = (email) => {
   console.log(email);
