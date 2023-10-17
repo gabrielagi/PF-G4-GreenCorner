00a41f43d9086de48278a3bc17943ed956e4023c
Franco Sosa <francososa1@hotmail.com>
2023-10-17 11:50:22 -0300

    ..

--- Server/src/Controller/payment.controller.js
+++ Server/src/Controller/payment.controller.js
@@ -19,12 +19,13 @@ mercadopago.configure({
 });
 
 const createOrder = async (req, res) => {
+  console.log(req.body);
   // El product puede ser un objeto individual desde Detail o un array desde Cart
-  const email = req.body.email;
-  console.log(req.body)
   const product = req.body.product;
   const amount = req.body.amount || 1; // Si amount no es enviado asumo un valor predeterminado en 1
   console.log("Este es el producto que me llega a payment: ", product);
+  const newEmail = req.body.email;
+  console.log("El email que me llega es: ", newEmail);
 
   // Guardo los items que se van a vender
   let items = [];
@@ -61,7 +62,7 @@ const createOrder = async (req, res) => {
       }
     }
   } else if (typeof product === "object") {
-    cartTotalAmount = product.price * amount
+    cartTotalAmount = product.price * amount;
     const availableStock = getAvailableStock(product.product_id, allProducts);
     if (availableStock <= amount) {
       insufficientStockProducts.push(product);
@@ -75,7 +76,7 @@ const createOrder = async (req, res) => {
     shippingAddress: "P. Sherman Calle Wallaby 42, Sidney",
     addressHouseNumber: 42,
     total: parseInt(cartTotalAmount),
-    email: email,
+    email: newEmail,
   };
   const newOrder = await postOrder(newOrderData);
   console.log("La nueva orden creada tiene ID: ", newOrder.dataValues.id);
@@ -193,18 +194,12 @@ const createOrder = async (req, res) => {
 
 const success = (req, res) => {
   console.log(req.query);
-
-//Get la ultima orden
-//Put status
-
-//Get el producto
-//Put el stock
-
+  console.log("Necesito en success");
   // res.send('Pago realizado')
   // store in database
   // Puedo guadar la información del usuario una vez que compró
   // Actualizar cantidad de productos en el Stock de los productos vendidos
-  res.redirect("http://localhost:5173/"); // Agregar componente notificación para redirigir
+  res.redirect("https://green-corner.vercel.app/"); // Agregar componente notificación para redirigir
 };
 
 const failure = (req, res) => {
@@ -213,29 +208,35 @@ const failure = (req, res) => {
   // store in database
   // Puedo guadar la información del usuario una vez que compró
   // Actualizar cantidad de productos en el Stock de los productos vendidos
-  res.redirect("http://localhost:5173/"); // Agregar componente notificación para redirigir si sale mal
+  res.redirect("https://green-corner.vercel.app/"); // Agregar componente notificación para redirigir si sale mal
 };
 
 const receiveWebhook = async (req, res) => {
-  // Recupero lo datos del pago que se realizó para poder ver el el id de la compra llamado data.id y el tipo llamado payment
-  const payment = req.query;
-
   try {
-    // Pregunto si la venta es correcta y la respuesta es payment
-    if (payment.type === "payment") {
-      const data = await mercadopago.payment.findById(payment.id);
-      console.log("Data del Webhook", data);
+    const { body } = req; // Obtén el cuerpo JSON de la solicitud
+
+    // Verifica que el tipo de notificación sea "payment"
+    if (body.type === "payment") {
+      const paymentId = body.data.id;
+
+      // Realiza acciones basadas en el ID del pago, como actualizar tu base de datos
+      // También puedes verificar el estado del pago, como "approved", "pending", "in_process", etc.
+
+      // Ejemplo de actualización en la base de datos (debes implementar esto):
+      // await actualizarEstadoDePago(paymentId, body.data.status);
+
+      console.log(
+        "Notificación de pago recibida:",
+        paymentId,
+        body.data.status
+      );
     }
 
-    res.status(204); // Significa que todo salió bien pero no devuelve nada
+    res.status(204).end(); // Responde con un estado 204 (sin contenido) para confirmar la recepción
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: error.message });
+    console.error("Error al procesar la notificación de Mercado Pago:", error);
+    res.status(500).json({ error: "Error interno" });
   }
-
-  console.log(req.query);
-  //res.redirect('url del front')
-  res.send("procesando pago");
 };
 
 module.exports = { createOrder, receiveWebhook, success, failure };
