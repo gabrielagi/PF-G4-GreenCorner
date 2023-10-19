3b0a427d0e1c5d3c365d9599057a4c222a643505
Julián Tejada <111610260+TomateComunista@users.noreply.github.com>
2023-10-18 21:17:18 -0500

    Testimonial by userId

--- Server/src/Controller/testimonial.controller.js
+++ Server/src/Controller/testimonial.controller.js
@@ -1,4 +1,4 @@
-const { Testimonial } = require("../db")
+const { Testimonial, User } = require("../db")
 const { getUserById } = require("../Controller/user.controller")
 const getAllTestimonials = async () => {
     const testimonials = await Testimonial.findAll()
@@ -15,6 +15,7 @@ const createTestimonial = async (message, date, rating, userId) => {
         }
 
         await testimonial.setUser(user);
+        return testimonial
         console.log("Testimonio creado");
     } catch (error) {
         console.log("Error al crear testimonio");
@@ -22,14 +23,37 @@ const createTestimonial = async (message, date, rating, userId) => {
     }
 };
 
-const getTestimonialbyId = async (id) => {
-    const testimonial = await Testimonial.findAll({
-        where: {
-            id: id
+// const getTestimonialbyId = async (id) => {
+//     const testimonial = await Testimonial.findAll({
+//         where: {
+//             userId: id
+//         }
+//     })
+//     return testimonial
+// }
+const getTestimonialbyId = async (userId) => {
+    try {
+        // Busca el usuario por su ID
+        const user = await User.findByPk(userId);
+
+        if (!user) {
+            throw new Error("User not found");
         }
-    })
-    return testimonial
-}
+
+        // Utiliza la función getTestimonial para obtener el testimonio asociado
+        const testimonial = await user.getTestimonial();
+
+        if (!testimonial) {
+            throw new Error("No testimonial found for this user");
+        }
+
+        // Ahora tienes el testimonio asociado al usuario
+        return testimonial;
+    } catch (error) {
+        console.error("Error al obtener testimonio por ID de usuario:", error);
+        throw error;
+    }
+};
 
 const deleteTestimonial = async (id) => {
     const testimonial = id;
